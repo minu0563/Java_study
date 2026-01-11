2165b8d2f8ab428f5f635aed64f90713e8b5a3f2
minu0563 <dgwag4977@gmail.com>
2026-01-11 12:45:34 +0900

    commit collection\iterable folder

---
+++ src/collection/iterable/MyArrayIterator.java
@@ -0,0 +1,17 @@
+package collection.iterable;
+
+public class MyArrayIterator {
+    /*
+    배열 리스트 set map
+
+    Iterable : 반복 가능한
+    Iterator : 반복자
+
+    boolean hasNext(); >> 다음 요소가 있는지 확인
+
+    */
+
+    public static void main(String[] args) {
+        
+    }
+}
